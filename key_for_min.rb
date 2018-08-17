01002ad3c993197146ac74487b3a676046ead22a
Jason Viger <jason.viger@gmail.com>
2018-08-17 03:45:31 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
    name_hash.each do |name, value|
      if min_name == ""
        min_name = name
-     else if name_hash[name] < value
+     elsif name_hash[name] < value
        min_name = name
      end
    end
